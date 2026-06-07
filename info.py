44bfe655e664dfa8e11fd0e33ca5211da33df619
Henock Joy <89825644+henockjoy@users.noreply.github.com>
2026-06-07 15:32:18 +0400

    Update default URL to include HTTPS

--- info.py
+++ info.py
@@ -139,7 +139,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "webtest-production-032b.up.railway.app")
+URL = environ.get("URL", "https://webtest-production-032b.up.railway.app/")
 if len(URL) == 0:
     logger.error('URL is missing, exiting now')
     exit()
